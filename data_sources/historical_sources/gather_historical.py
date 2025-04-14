3a7b2f01c379a0003058e6c37d1d06a19ac6979f
John Schmitt <jjshutout19@yahoo.com>
2025-04-14 15:28:21 -0500

    fixed csv formatting, saved financial data as a csv file

--- data_sources/historical_sources/gather_historical.py
+++ data_sources/historical_sources/gather_historical.py
@@ -33,10 +33,10 @@ for row in rows:
 
         sp100_list.append(temp_dict)
 
-# have df of sp100 companies in dr
-
+# have listz of sp100 companies
 df = pd.DataFrame()
 
+# get last year of data
 end_time = datetime.now()
 start_time = end_time - timedelta(days=365)
 
@@ -44,29 +44,37 @@ all_data = []
 
 
 for company in sp100_list:
-    ticker = company['ticker']
+    # account for error when retreiving Berkshire hathway data
+    ticker = company['ticker'] if company['ticker'] != "BRK.B" else "BRK-B"
     company_name = company['company_name']
 
     try:
         print(f"Fetching {ticker}")
 
-        df = yf.download(ticker, start=start_time.strftime('%Y-%m-%d'), end=end_time.strftime('%Y-%m-%d'))
+        # get info from yfinance
+        df = yf.download(ticker, start=start_time.strftime('%Y-%m-%d'), end=end_time.strftime('%Y-%m-%d'), multi_level_index=False)
 
         df.reset_index(inplace=True)
-        
-        df = df[["Date", "Close", "High", "Low", "Open", "Volume"]]
-        df["ticker"] = ticker
-        df["company_name"] = company_name
 
+        #set ticker and company_name cols
+        df['ticker'] = ticker
+        df['company_name'] = company_name
+            
         all_data.append(df)
-
+        
     except Exception as e:
         print("Failed to get data")
 
 
+# concat all of the data into a final df and rename cols
 final_df = pd.concat(all_data, ignore_index=True)
 final_df.rename(columns={"Date": "date", "Close": "close", "High": "high",
                          "Low": "low", "Open": "open", "Volume": "volume"}, inplace=True)
+
+# rearrange columns so ticker and company name are first
+last_cols = final_df.iloc[:, -2:]
+final_df = final_df.iloc[:, :-2]
+final_df = pd.concat([last_cols, final_df], axis=1)
 
 final_df.to_csv(csv_path)
 
