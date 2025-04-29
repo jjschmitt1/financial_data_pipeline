818b4e0f3ad832dc6f471c976a8304e53a7d9664
John Schmitt <jjshutout19@yahoo.com>
2025-04-29 17:12:55 -0500

    added sqlite functionality and put the data in for yesterday from them

--- etl/create_and_load_daily_data.sh
+++ etl/create_and_load_daily_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# get daily data from yfinance
 python3.12 extract_daily_data.py
-
 if [ $? -ne 0 ]; then
   echo "Error extracting daily data"
   exit 1
@@ -9,12 +9,22 @@ else
   echo "daily data extracted"
 fi
 
+# load data into duckdb
 python3.12 load_duck.py
 if [ $? -ne 0 ]; then
   echo "Error loading into duckdb"
   exit 1
 else
   echo "successfully loaded daily data into duckdb"
+fi
+
+# load data into sqlite
+python3.12 load_sqlite.py
+if [ ?$ -ne 0 ]; then
+  echo "Error loading into sqlite"
+  exit 1
+else
+  echo "successfully loaded daily data into sqlite"
 fi
 
 echo "Processed finished without error"
---
+++ etl/load_sqlite.py
@@ -0,0 +1,14 @@
+import sqlite3
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+conn = sqlite3.connect(os.getenv("SQLITE_DB"))
+
+df = pd.read_csv(os.getenv("DAILY_DATA_CSV"))
+df.to_sql('financial_data', conn, if_exists="append", index=False)
+
+conn.commit()
+conn.close()
